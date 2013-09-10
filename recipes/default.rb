80ef134a99dda49936a8b28929fa74ebdf7c83c1
Ivan Carlos Da Silva Lopes <ivanlopes@42algoritmos.com.br>
2013-09-10 01:29:47 -0300

    Update para versão 0.1.2

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,10 @@
 # recipes/default.rb
+
 template "#{ENV['HOME']}/hello-world.txt" do
   source 'hello-world.txt.erb'
-  mode '0644'
+  mode '0664'
+end
+
+%w(build-essential curl).each do |pkg|
+  package pkg
 end
